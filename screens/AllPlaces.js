ed54d418779920d901ca851b8ea4dfac29b9d1e2
Mohamad Sharafeddine <mhsharafeddine@gmail.com>
2025-01-07 02:52:48 +0200

    Enhanced PlaceItem and PlacesList components with improved styling and layout. Updated AllPlaces to manage loaded places based on navigation focus.

--- screens/AllPlaces.js
+++ screens/AllPlaces.js
@@ -1,9 +1,20 @@
 import { StyleSheet, Text, View } from "react-native";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import PlacesList from "../components/Places/PlacesList";
+import { useIsFocused } from "@react-navigation/native";
 
-export default function AllPlaces() {
-  return <PlacesList />;
+export default function AllPlaces({ route }) {
+  const [loadedPlaces, setLoadedPlaces] = useState([]);
+  const isFocused = useIsFocused();
+  useEffect(() => {
+    if (!isFocused && route.params) {
+      setLoadedPlaces((currentPlaces) => [
+        ...currentPlaces,
+        route.params.place,
+      ]);
+    }
+  }, [isFocused, route]);
+  return <PlacesList places={loadedPlaces} />;
 }
 
 const styles = StyleSheet.create({});
